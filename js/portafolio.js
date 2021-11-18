82e6326b01c6ccf60b167ece13c05ef6463d2c33
ubcruzl <40645692+ubcruzl@users.noreply.github.com>
2021-11-17 22:13:55 -0800

    CHANGES PORTAFOLIO

--- js/portafolio.js
+++ js/portafolio.js
@@ -7,6 +7,7 @@ function closeForm() {
 }
 // This function displays the first image in the slideshow when the page loads
 var slideIndex = 1;
+var timeOutId;
 showSlides(slideIndex);
 
 // This function changes the slide when the left or right arrows are clicked
@@ -18,9 +19,12 @@ function plusSlides(n) {
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
+
 function showSlides(n) {
+    var i;
     var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
     var dots = document.getElementsByClassName("dot"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
+    if (n==undefined){n = ++slideIndex;}
     if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
     if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
     for (i = 0; i < slides.length; i++) {
@@ -31,4 +35,5 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
     dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
+    timeOutId = setTimeout(showSlides, 3000); // Change image every 8 seconds
 }
